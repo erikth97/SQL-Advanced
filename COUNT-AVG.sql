dbbd96e752cbd1bacf6a55042fcf87e920cc618d
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-12-22 15:32:47 -0600

    Update COUNT-AVG.sql

--- COUNT-AVG.sql
+++ COUNT-AVG.sql
@@ -1,4 +1,4 @@
-
+------------ COUNT -----------
 SELECT
 	a.EmailPromotion,
 	COUNT(a.EmailPromotion) AS Cantidad
@@ -23,3 +23,17 @@ SELECT
 	COUNT(DISTINCT a.LastName) AS Cantidad
 FROM
 	person.Person AS a
+
+
+
+----------- AVG -------------------
+
+SELECT 
+	a.Color,
+	AVG(a.listPrice) AS Promedio
+FROM
+	Production.Product AS a
+GROUP BY
+	a.Color
+
+
